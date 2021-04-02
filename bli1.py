e7efa846cf30c8a2b30f0bb95a392a6f06492c4c
jzy67 <81802773+jzy67@users.noreply.github.com>
2021-04-02 16:56:34 +0800

    Add files via upload

---
+++ bli1.py
@@ -0,0 +1,11 @@
+name_list=["ji","sd","yu"];
+# print(name_list[0]);
+# name_list[2]="空";
+# print(name_list);
+# for name in name_list:
+#     print(f"{name}");
+num=name_list.index("sd");
+print(num);
+name_list.append("zh");
+name_list.insert(1,"SD");
+print(name_list);
